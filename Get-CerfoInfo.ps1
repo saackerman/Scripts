c152eef864c1466b53b64a24acdd46306de8b293
Sean Ackerman <saackerman@users.noreply.github.com>
2023-11-02 12:54:36 -0700

    Create Get-CerfoInfo.ps1
    Get cert info , useful for validating cert expiration rotation validation. Feel free to use in your CI/CD pipeline or adhoc as needed.

---
+++ Get-CerfoInfo.ps1
@@ -0,0 +1,51 @@
+<#
+.DESCRIPTION
+gets certificate info. Add fields you want as needed. gets cert thumbprint, name, and start/end date. calculates days until it expires.
+
+.PARAMETER url
+Enter url , default is good if you enter nothing
+
+.PARAMETER port
+Enter port default is 443
+
+.OUTPUTS
+$certInfo will contain for example
+Hostname       : www.google.com
+Thumbprint     : F5CCDAB5BA1E141444CC279092CC601F5F08AF77
+Start          : 10/16/2023 01:10:46
+End            : 01/08/2024 01:10:45
+ExpirationDays : 66
+
+.NOTES
+Used AI to clean up code.
+VScode editor.
+
+.Link
+https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.tcpclient?view=net-7.0
+
+.FUNCTIONALITY
+Cool huh kids.
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$url = "www.google.com",
+    [int]$port = "443"
+)
+
+$request = [System.Net.Sockets.TcpClient]::new($url, $port)
+$stream = [System.Net.Security.SslStream]::new($request.GetStream())
+$stream.AuthenticateAsClient($url)
+$effectiveDate = $stream.RemoteCertificate.GetEffectiveDateString() -as [datetime]
+$expirationDate = $stream.RemoteCertificate.GetExpirationDateString() -as [datetime]
+# create custom object, adjust parameters as needed
+$certInfo = [pscustomobject] @{
+    Hostname        = $stream.TargetHostName
+    Thumbprint      = $stream.RemoteCertificate.Thumbprint
+    Start           = [string]$effectiveDate
+    End             = [string]$expirationDate
+    ExpirationDays  = (New-TimeSpan -Start (Get-Date) -End $expirationDate).Days
+}
+$certInfo
